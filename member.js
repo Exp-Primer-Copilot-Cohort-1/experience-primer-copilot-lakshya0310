b5223c439e2e134056340237f1764cfaecd8ff9c
Lakshya Jindal <44470850+lakshya0310@users.noreply.github.com>
2023-10-26 20:24:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+}
